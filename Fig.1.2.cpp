b9ac8ff2b557b046a8c18df636e379720947668c
Luís Ricardo do Rosário Silva <lovewoodstar@gmail.com>
2023-09-27 22:44:10 -0300

    Add files via upload

---
+++ Fig.1.2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int dias;
+    cout << "Numero de dias: ";
+    cin >> dias;
+
+    int horas = dias * 24;
+    cout << dias << " dias tem " << horas << " horas ." << endl;
+    
+    return 0;
+}
+
+
