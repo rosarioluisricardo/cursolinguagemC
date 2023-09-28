eac62505cfa6737f04f22645c8c93131765d18c9
Luís Ricardo do Rosário Silva <lovewoodstar@gmail.com>
2023-09-27 22:43:09 -0300

    Add files via upload

---
+++ Fig. 1.6  fig01_06.cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int integer1, integer2, sum;
+
+	cout << "Digite o primeiro inteiro\n" ;
+	cin >> integer1;
+
+	cout << "Digite o segundo inteiro\n ";
+	cin >> integer2;
+
+	sum = integer1 + integer2;
+
+	cout << "A soma eh " << sum << endl;
+
+	return 0;
+
+}
